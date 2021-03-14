44513dfa57b40dda0223f5a1ebd9c0293a8047c8
joshuamakula <makulajosh@gmail.com>
2021-03-14 15:48:20 +0300

    added li list using javascript

---
+++ app.js
@@ -0,0 +1,38 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event listeners
+todoInput.addEventListener('click', addTodo)
+
+// Functions
+function addTodo(event) {
+    // Prevent form from submitting 
+    event.preventDefault();
+
+    // Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    // Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = "Hey";
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    // CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    // CHECK trash BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('complete-btn');
+    todoDiv.appendChild(trashButton);
+
+    // APPEND TO LIST
+    todoList.appendChild(todoDiv)
+}
