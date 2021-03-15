b1afa5a605ff5357c9d61d2786cb37a80852c0d8
joshuamakula <makulajosh@gmail.com>
2021-03-15 15:46:58 +0300

    make the filter display lists as required

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // Functions
 function addTodo(event) {
@@ -60,4 +62,29 @@ function deleteCheck(e) {
         todo.classList.toggle('completed');
     }
 
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break; 
+        }
+    });
 }
